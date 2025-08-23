0e47920c523c061d10676161f0878596c561b528
rafaellamelodev <contatorafaellamelo@gmail.com>
2025-08-23 10:55:58 -0300

    Update DatabaseConnection.java
    mudança nome banco de dados

--- mavenproject1/src/main/java/connections/DatabaseConnection.java
+++ mavenproject1/src/main/java/connections/DatabaseConnection.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
  */
 public class DatabaseConnection {
      private static final String URL = 
-            "jdbc:mysql://localhost:3306/bd_jschool";     
+            "jdbc:mysql://localhost:3306/jschool";     
     private static final String USER = "root";
     private static final String SENHA = "admin";
     private static final String teste = "teste";
