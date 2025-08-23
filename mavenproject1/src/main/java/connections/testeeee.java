cfc9519b42867f2f91aa0b5cf97c18d03e8e0191
Gladyston1982 <gktlopes@gmail.com>
2025-08-23 11:43:07 -0300

    Update testeeee.java

--- mavenproject1/src/main/java/connections/testeeee.java
+++ mavenproject1/src/main/java/connections/testeeee.java
@@ -10,7 +10,7 @@ package connections;
  */
 public class testeeee {
     public static void main(String[] args) {
-        System.out.println("Hello World22222333333");
+        System.out.println("O Mito");
     }
     
 }
