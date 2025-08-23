c331c5f414268576327e36dc08e7674c06d7fe42
Gladyston1982 <gktlopes@gmail.com>
2025-08-23 11:46:38 -0300

    socorroDeus

--- mavenproject1/src/main/java/connections/testeeee.java
+++ mavenproject1/src/main/java/connections/testeeee.java
@@ -11,6 +11,7 @@ package connections;
 public class testeeee {
     public static void main(String[] args) {
         System.out.println("O Mitoooo");
+        System.out.println("socorro Deus");
     }
     
 }
