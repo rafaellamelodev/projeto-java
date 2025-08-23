23417ac08fc1f676acb54046a65aac8901f689e0
rafaellamelodev <contatorafaellamelo@gmail.com>
2025-08-23 16:51:05 -0300

    aa

--- mavenproject1/src/main/java/src/ClassManagement.java
+++ mavenproject1/src/main/java/src/ClassManagement.java
@@ -4,10 +4,41 @@
  */
 package src;
 
+import connections.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.mindrot.jbcrypt.BCrypt;
+import connections.*;
+
+import java.util.*;
+
 /**
  *
  * @author D18_11
  */
 public class ClassManagement {
     
+    
+   public List<String> listarDisciplinas() throws SQLException {
+       
+    List<String> disciplinas = new ArrayList<>();
+    String query = "SELECT nome FROM disciplinas";
+    
+    try (Connection conexao = DatabaseConnection.getConnection();
+         PreparedStatement stmt = conexao.prepareStatement(query);
+         ResultSet rs = stmt.executeQuery()) {
+
+        while (rs.next()) {
+            disciplinas.add(rs.getString("nome"));
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+
+    return disciplinas;
+}
 }
