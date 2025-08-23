3eab10a6fa2b9913b52156eb9fa0eb4f24ea95b3
rafaellamelodev <contatorafaellamelo@gmail.com>
2025-08-23 10:40:55 -0300

    teste

---
+++ mavenproject1/src/main/java/connections/DatabaseConnection.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package connections;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author D18_11
+ */
+public class DatabaseConnection {
+     private static final String URL = 
+            "jdbc:mysql://localhost:3306/bd_jschool";     
+    private static final String USER = "root";
+    private static final String SENHA = "admin";
+    
+    
+    /*método para tentar conectar ao banco de dados
+    em caso de falha, uma exceção é lançada*/
+public static Connection getConnection() throws SQLException {
+    return DriverManager.getConnection(URL, USER, SENHA);
+}
+}
